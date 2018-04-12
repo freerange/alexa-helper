960dfa858cac1f43b0375d372178e78e8cdfac97
Chris Lowis <chris.lowis@gmail.com>
2018-04-12 15:51:04 +0100

    Add test for adding chosen person to session
    As we are reusing the event context we clear out the
    `session.attributes` before each test.

--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 const index = require('./index');
 
-test('Triggering the assignment intent says the name of the chosen person', () => {
+describe('The assignment intent', () => {
   const event = { 'request': { 'locale': 'en-US' },
                   'session': { 'application': { 'applicationID': 'application-id' },
                                'user': { 'userId': 'user-id' } },
@@ -8,8 +8,21 @@ test('Triggering the assignment intent says the name of the chosen person', () =
   const context = { };
   const callback = jest.fn();
 
-  index.handler(event, context, callback);
+  beforeEach(() => {
+    event.session.attributes = { };
+  });
 
-  const result = callback.mock.calls[0][1];
-  expect(result.response.outputSpeech.ssml).toMatch(/\<speak\> .+ is the chosen one \<\/speak\>/)
+  test('it says the name of the chosen person', () => {
+    index.handler(event, context, callback);
+
+    const result = callback.mock.calls[0][1];
+    expect(result.response.outputSpeech.ssml).toMatch(/\<speak\> .+ is the chosen one \<\/speak\>/)
+  });
+
+  test('it adds the name of the chosen person to the session', () => {
+    index.handler(event, context, callback);
+
+    const result = callback.mock.calls[0][1];
+    expect(result.sessionAttributes.assigned.length).toBe(1)
+  });
 });
